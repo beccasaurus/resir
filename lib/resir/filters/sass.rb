4be7d9f7a2e8057a133bc4011a8bf8a3d5157048
remi <remi@remitaylor.com>
2008-01-15 04:44:21 -0700

    updated sass to set Content-Type to text/css

--- lib/resir/filters/sass.rb
+++ lib/resir/filters/sass.rb
@@ -2,5 +2,6 @@ if lib_available? 'haml'
   require 'resir/filters/erb'
   Resir.filters.sass = lambda { |text,binding| 
     eval( '@layout = nil', binding ) # disable layout
+    eval( %{response.header['Content-Type'] = 'text/css'}, binding ) # set to text/css
     Sass::Engine.new( Resir.filters.erb[text,binding] ).render }
 end
