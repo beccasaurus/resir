2960dea1e4a08eebd7dcc14840ed75b0e493df5c
remi <remi@remitaylor.com>
2008-02-20 12:27:42 -0700

    it's pretty gross with all of the lambdas instead of methods and ... 'real code' ... layouts are working tho.  and partials should only take a sec.  once partials are working, i *might* try re-doing it, OO, to see if i can compete with the line count.  i might be able to ... yeah.  i'll try re-doing with OO in a sec

--- resir-tiny/resir-tiny.rb
+++ resir-tiny/resir-tiny.rb
@@ -5,6 +5,17 @@ dir     = File.expand_path( ARGV.shift || '.' )
 filters = {
   'erb' => lambda { |text,b| require 'erb'; ERB.new(text).result(b) }
 }
+find_file = lambda { |path| Dir[ File.join(dir,path) + '*' ].first }
+name_exts = lambda { |file| 
+  exts = File.basename( file ).split '.'
+  name = exts.shift
+  name += ".#{exts.first}" unless exts.empty?
+  return name, exts
+}
+render_with_exts = lambda { |text, exts, binding| 
+  exts.map { |ext| filters[ext] }.compact.each { |f| text = f.call text, binding }
+  text
+}
 
 Thin::Server.start('0.0.0.0', 5000) do 
   use Rack::CommonLogger
@@ -13,20 +24,23 @@ Thin::Server.start('0.0.0.0', 5000) do
   run lambda { |env|
     request  = Rack::Request.new env
     response = Rack::Response.new
+    layout   = 'layout'
 
     path = (env['PATH_INFO'] == '/') ? '/index' : env['PATH_INFO']
     path = path + '.html' unless path.include? '.'
-    file = Dir[ File.join(dir,path) + '*' ].first
+    file = find_file.call path
     return [ 404, {}, "File Not Found: #{path}" ] unless file
 
-    exts = File.basename( file ).split '.'
-    name = exts.shift ; name += ".#{exts.first}" unless exts.empty?
+    name, exts = name_exts.call file
     response.headers['Content-Type'] = (MIME::Types.type_for(name) || 'text/plain').to_s
+    response.body = render_with_exts.call File.read(file), exts, binding
 
-    response.body = File.read file
-    exts.map { |ext| filters[ext] }.compact.each { |f| response.body = f.call response.body, binding }
+    layout = find_file.call layout if layout
+    if layout and response.headers['Content-Type'] == 'text/html'
+      content = response.body
+      response.body = render_with_exts.call File.read(layout), name_exts[layout].last, binding
+    end
 
     response.finish
   }
 end
-
